f49603f1f9f848ea7bdfa14715560cd6e397b1ce
stone0351 <stone0351@gmail.com>
2025-03-04 19:30:51 +0800

    从terraform的源代码里面删除 cloudfront 和 route53相关的操作，以免下次运行时覆盖了我手工完成的设置

--- terraform/main.tf
+++ terraform/main.tf
@@ -243,51 +243,6 @@ resource "aws_api_gateway_resource" "execute_resource" {
   path_part   = "execute"
 }
 
-# Route53 configuration for custom domain
-resource "aws_route53_zone" "main" {
-  name = "getmemap.com"
-}
+# Route53 configuration has been manually set up and is no longer managed by Terraform
 
-# Route53 record removed - using CloudFront default domain name instead
-
-# CloudFront distribution for HTTPS
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    origin_id   = "S3-ssp-getmemap-com"
-  }
-  
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-  
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-ssp-getmemap-com"
-    
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-    
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-  
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-  
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
-
-# SSL Certificate removed - using CloudFront default certificate instead
+# CloudFront distribution has been manually set up and is no longer managed by Terraform
